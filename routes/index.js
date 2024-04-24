ff7b6f9543937de3d1f2b5c41d89334700315376
guilbert <guilbertmartin75@gmail.com>
2024-04-24 14:11:52 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+//ajouter variable 
+const apiKey = process.env.API_KEY;
+
+router.get('/movies', function(req, res){
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => res.json(data.results))
+})
+
+module.exports = router;
---
+++ routes/movies.js
@@ -0,0 +1,10 @@
+// var express = require('express');
+// var router = express.Router();
+
+// router.get('/', function(req, res){
+//     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=d393a84e13b5db708574d8ee8abf9635`)
+//         .then(response => response.json())
+//         .then(data => res.json(data))
+// })
+
+// module.exports = router;
